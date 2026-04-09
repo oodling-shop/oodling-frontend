fe6482997740b51a7499cf1023e22804bbe3c649
Aswin-Venu-M <aswinvg001@gmail.com>
2026-04-09 15:28:37 +0530

    feat: add compiler configuration to remove console logs in production

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@phosphor-icons/react'],
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.shopify.com' },
